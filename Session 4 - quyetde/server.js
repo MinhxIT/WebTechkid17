fb1147798d037c2ac9a84746eb7383df7b7f4c57
MinhxIT <varanenguyen@gmail.com>
2018-12-18 08:46:20 +0700

    btvn ss4

--- Session 4 - quyetde/server.js
+++ Session 4 - quyetde/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express(); // app là web server
-
+app.use(express.static(__dirname+'/resource/'));
+app.get("/",(request,response)=>{
+    // response.send(JSON.stringify({a:5,b:7}));
+   // response.send(JSON.stringify({a:5,b:7}));
+    response.send("<h1>Helloo</h1>");
+    //response.sendFile(__dirname + "/resource/index1.html");
+});
+app.get("/about",(req,res)=>{
+    res.sendFile(__dirname + "/resource/about.html");
+});
 app.listen(3000,(err)=>{
     if(err){
         console.log(err);
