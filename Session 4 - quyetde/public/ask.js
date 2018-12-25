a284872ed6d70c8df0e2354aecd4ab169382980a
MinhxIT <varanenguyen@gmail.com>
2018-12-25 16:11:51 +0700

    Ss 7 -Jquery - loi tu update id cau hoi

--- Session 4 - quyetde/public/ask.js
+++ Session 4 - quyetde/public/ask.js
@@ -1,6 +1,40 @@
-var content = document.getElementById("content").addEventListener("input",function(){
-    var check = document.getElementById("count").innerText = 200 - document.getElementById("content").value.length;
-    if(check<0){
-        document.getElementById("content").value.slice(0,199);
-    }
+
+
+// var content = document.getElementById("content").addEventListener("input",function(){
+//     var check = document.getElementById("count").innerText = 200 - document.getElementById("content").value.length;
+//     if(check<0){
+//         document.getElementById("content").value.slice(0,199);
+//     }
+// });
+// jquery 
+$(document).ready(function () {
+    $('#content').on('input', function () {
+        $('#count').text(200-$('#content').val().length);
+    });
+    
+    $(".btn-answer").click(function () { 
+        const questionId = $(".btn-answer").attr("data-questionId");
+        const questionVote = $(this).attr("id");
+        $.ajax({
+            type: "GET",
+            url: "/vote/"+questionId+"/" + questionVote,
+            success: function (data) {
+                window.location.href = "/answer";
+            },
+            error:function(err){
+                console.log(err);
+            }
+        });
+    });
+    $.ajax({
+        type: "GET",
+        url: "/api/random",
+        success: function (data) {
+            $("#contentQues").text(data.question.content);
+            $(".btn-answer").attr("data-questionId",data.question.id);
+        },
+        error:function(err){
+            console.log(err);
+        }
+    });
 });
--- Session 4 - quyetde/server.js
+++ Session 4 - quyetde/server.js
@@ -3,47 +3,52 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express(); // app là web server
 
-app.use(express.static(__dirname+'/public/'));//
+app.use(express.static(__dirname+'/public'));//
 app.use(bodyParser.urlencoded({extended:false}));
-app.get("/",(req,res)=>{
+
+app.get("/answer",(req,res)=>{
     // response.send(JSON.stringify({a:5,b:7}));
    // response.send(JSON.stringify({a:5,b:7}));
    const questions = JSON.parse(fs.readFileSync("./questionAsk.json",{encoding:"utf-8"}));
    if(questions.length==0){
         res.send("Chưa có câu hỏi nào");
    }else{
-        
-        const randomQuestion = questions[Math.floor(Math.random()*questions.length)]
-        res.send(`<h1>
-            ${randomQuestion.content}
-        </h1>
-        <form action="/countVote/${randomQuestion.id}" method="POST">
-            <button name="vote" value="yes">Đúng/Có/Phải</button>
-            <button name="vote" value="no">Sai/không/Trái</button>
-        </form>
-        `);
+        const randomQuestion = questions[Math.floor(Math.random()*questions.length)];
+        // res.send(`<h1>
+        //     ${randomQuestion.content}
+        // </h1>
+        // <a href="/vote/${randomQuestion.id}/yes">Yes</a><br>
+        // <a href="/vote/${randomQuestion.id}/no">No</a><br>
+        // `);
+        res.sendFile(__dirname +"/view/answer.html");
    }
    
 });
+app.get("/api/random",(req,res)=>{
+    const questions = JSON.parse(fs.readFileSync("./questionAsk.json",{encoding:"utf-8"}));
+    const randomQuestion = questions[Math.floor(Math.random()*questions.length)]
+    res.send({question:randomQuestion});
+})
+
 // app.get("/about",(req,res)=>{
 //     res.sendFile(__dirname + "/resource/about.html");
 // }); // đến trang about
-app.post("/countVote/:idQues",(req,res)=>{
-    let vote = req.body.vote;
-    let idQues = req.params.idQues; // tham số id câu hỏi
-    const questions = JSON.parse(fs.readFileSync("./questionAsk.json",{encoding:"utf-8"})); // đọc dữ liệu ra
-    questions.forEach((question) => {
-        if(question.id == idQues){
-            if(vote=="yes"){
-                question.yes++;
-            }else if(vote=="no"){
-                question.no++;
-            }
-        }
-    });
-    fs.writeFileSync("./questionAsk.json",JSON.stringify(questions)); // ghi vào 
-    res.redirect("/")
-});
+// app.post("/countVote/:idQues",(req,res)=>{
+//     let vote = req.body.vote;
+//     let idQues = req.params.idQues; // tham số id câu hỏi
+//     const questions = JSON.parse(fs.readFileSync("./questionAsk.json",{encoding:"utf-8"})); // đọc dữ liệu ra
+//     questions.forEach((question) => {
+//         if(question.id == idQues){
+//             if(vote=="yes"){
+//                 question.yes++;
+//             }else if(vote=="no"){
+//                 question.no++;
+//             }
+//         }
+//     });
+//     fs.writeFileSync("./questionAsk.json",JSON.stringify(questions)); // ghi vào 
+//     res.redirect("/")
+// });
 app.get("/ask",(req,res)=>{
     res.sendFile(__dirname + "/view/ask.html");
 }); // đến trang ask 
@@ -58,7 +63,24 @@ app.post("/addQuestion",(req,res)=>{
     }; // dữ liệu mới
     questions.push(newQuestion); // thêm dữ liệu vào json 
     fs.writeFileSync("./questionAsk.json",JSON.stringify(questions)); // ghi vào 
-    res.redirect('/');
+    res.redirect('/answer');
+});
+app.get("/vote/:questionId/:vote",(req,res)=>{
+    const questionId = req.params.questionId;
+    const vote = req.params.vote;
+    let questions = JSON.parse(fs.readFileSync("./questionAsk.json"),{encoding:"utf-8"});
+    questions.forEach((question,index)=>{
+        if(question.id = questionId){
+            if(vote=="yes"){
+                questions[index].yes+=1;
+            }
+            else{
+                questions[index].no+=1;
+            }
+        }
+    });
+    fs.writeFileSync("./questionAsk.json",JSON.stringify(questions));
+    res.redirect("/ask");
 });
 app.listen(3000,(err)=>{
     if(err){
